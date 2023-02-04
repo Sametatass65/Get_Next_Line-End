303d6a88a10c8496dc741b4f16e05e93b7f37985
Sametatass65 <sametatass65@gmai.com>
2023-02-04 19:22:07 +0300

    .c file added

--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 19:00:31 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/04 19:00:31 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
 #include <unistd.h>
 #include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 unsigned int	ft_strlen(char *ptr);
 char			*ft_strchr(char *s, int c);
