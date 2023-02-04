e33204215ffa8f1138d1e35bf27366e782bad54b
Sametatass65 <sametatass65@gmai.com>
2023-02-04 18:55:39 +0300

    .h and ulilts added

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+unsigned int	ft_strlen(char *ptr);
+char			*ft_strchr(char *s, int c);
+char			*ft_strdup(char *s1);
+char			*ft_substr(char *s, unsigned int start, unsigned int len);
+char			*ft_strjoin(char *s1, char *s2);
+
+
+#endif
